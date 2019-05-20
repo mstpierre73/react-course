9170e95173607f3610cebd7881e2c011a41f9b22
mstpierre73 <naranjaqc@hotmail.com>
2019-05-20 14:37:29 -0400

    add defaultProps to project

--- ringo-paul/index.js
+++ ringo-paul/index.js
@@ -8,7 +8,10 @@ class App extends React.Component {
                 bangs={4}
                 img="https://images.unsplash.com/photo-1557562440-b67d58679232?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
                 />
-                
+                <Hello 
+                to="Georges" 
+                img="https://images.unsplash.com/photo-1557562440-b67d58679232?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+                />
             </div>
         )
     }
