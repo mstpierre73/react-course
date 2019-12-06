b25035c3bb69b4b681267253e0e1ac898aae41ae
Marise St-Pierre <naranjaqc@hotmail.com>
2019-12-05 19:57:39 -0500

    creating event handler

--- state-demo/state-demo/src/App.js
+++ state-demo/state-demo/src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import Game from './Game';
 import Rando from './Rando';
+import Button from './Button';
+import BrokenClick from './BrokenClick';
 
 class App extends Component {
   render() {
@@ -8,6 +10,8 @@ class App extends Component {
       <div className="App">
         <Game player="Mimi" session={2}/>
         <Rando maxNum={7}/>
+        <Button />
+        <BrokenClick />
       </div>
     )
   }
