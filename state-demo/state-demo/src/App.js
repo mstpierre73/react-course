eab912ef05f5b29c43d83bd83abd30dacd004646
Marise St-Pierre <naranjaqc@hotmail.com>
2019-12-05 19:33:34 -0500

    Changing states

--- state-demo/state-demo/src/App.js
+++ state-demo/state-demo/src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import Game from './Game';
+import Rando from './Rando';
 
 class App extends Component {
   render() {
     return(
       <div className="App">
         <Game player="Mimi" session={2}/>
+        <Rando maxNum={7}/>
       </div>
     )
   }
