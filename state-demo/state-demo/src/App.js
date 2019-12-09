23a0a7a6c81435b28988f93be4891c56d093ec28
Marise St-Pierre <naranjaqc@hotmail.com>
2019-12-08 20:55:05 -0500

    add conditionnal

--- state-demo/state-demo/src/App.js
+++ state-demo/state-demo/src/App.js
@@ -1,17 +1,11 @@
 import React, {Component} from 'react';
-import Game from './Game';
-import Rando from './Rando';
-import Button from './Button';
-import BrokenClick from './BrokenClick';
+import Clicker from './Clicker';
 
 class App extends Component {
   render() {
     return(
       <div className="App">
-        <Game player="Mimi" session={2}/>
-        <Rando maxNum={7}/>
-        <Button />
-        <BrokenClick />
+        <Clicker />
       </div>
     )
   }
---
+++ state-demo/state-demo/src/Clicker.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+
+class Clicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            num : 1
+        }
+        this.genRandom = this.genRandom.bind(this);
+    }
+
+    genRandom(){
+        let rand = Math.floor(Math.random() * 10) + 1;
+        this.setState({num: rand});
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>My number is : {this.state.num}</h1>
+                {this.state.num === 7
+                ?
+                <h2>You Win!</h2> :
+                <button onClick={() => {
+                    this.genRandom();
+                }}>Click Me!</button>
+            }
+            </div>
+            )
+        }
+}
+
+export default Clicker;
